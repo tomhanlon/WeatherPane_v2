de65046fe45d19040506c697f4fdaa7cceae091f
Tom Hanlon <tomhanlon@gmail.com>
2013-12-07 17:06:34 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WeatherPane::Application.config.secret_key_base = '5669488b7b01b8c3d818e3698a9532239d73e8ccbd832f62ec94c50fb71ff1a7ed876ca7fe3ad13dae70220b41149300123fd2293f2536af7913fd0f0556ff81'
