467df5a117690a0c07f63c2376a7a6b4b0b44ab0
Tejas Rawal <trawal90@gmail.com>
2013-12-07 18:06:06 -0600

    Add api function to controller

--- Gemfile
+++ Gemfile
@@ -15,8 +15,6 @@ gem 'rails', '4.0.1'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 
-gem 'figaro'
-
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
 
--- app/controllers/conditions_controller.rb
+++ app/controllers/conditions_controller.rb
@@ -3,4 +3,23 @@ class ConditionsController < ApplicationController
   def index
   end
 
+  def condition_query
+		response = Unirest.get("https://api.forecast.io/forecast/89c4c48896b556b333f12c324914e8f1/30.2500,-97.7500")
+	 	@current_condition = response.body['currently']['icon']
+	 	@current_temp = response.body['currently']['temperature'] 
+
+	 	case @current_condition
+		 	when 'rain' then @icon = 'images/rain.jpg'
+		 	when 'clear-day' then @icon = 'clear-day.jpg'
+		 	when 'clear-night' then @icon = 'clear-night.jpg'
+		 	when 'cloudy' then @icon = 'cloudy.jpg'
+		 	when 'fog' then @icon = 'fog.jpg'
+		 	when 'partly-cloudy-night' then @icon = 'partly-cloudly-night.jpg'
+		 	when 'partly-cloudy-day' then @icon = 'partly-cloudy-day.jpg'
+		 	when 'sleet' then @icon = 'sleet.jpg'
+		 	when 'snow' then @icon = 'snow.jpg'
+		 	when 'wind' then @icon = 'wind.jpg'
+		 	else
+	 	end 
+	end	
 end
