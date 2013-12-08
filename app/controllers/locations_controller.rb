707cf6148fd55dfadd70f217e3cc7fe9218cb51f
Tom Hanlon <tomhanlon@gmail.com>
2013-12-08 12:46:54 -0600

    Add lat and long functionality to URL of show view

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -4,9 +4,25 @@ class LocationsController < ApplicationController
   end
 
   def show
-    response = Unirest.get("https://api.forecast.io/forecast/#{Figaro.env.STRIPE_API_KEY}/30.2500,-97.7500")
+    controller_lat = params[:latitude]
+    controller_long = params[:longitude]
+
+    new1 = controller_lat.gsub('x', '.')
+    new2 = controller_long.gsub('x', '.')
+
+    if new2[0] == 'n'
+      new2 = new2.gsub('n','-')
+    elsif new1[0] =='n'
+      new1 = new1.gsub('n','-')
+    end
+
+    response = Unirest.get("https://api.forecast.io/forecast/#{Figaro.env.STRIPE_API_KEY}/#{new1},#{new2}")
     @current_condition = response.body['currently']['icon']
     @current_temp = response.body['currently']['temperature']
+
+    place = Unirest.get("http://maps.googleapis.com/maps/api/geocode/json?latlng=#{new1},#{new2}&sensor=true")
+    @current_city = place.body['results'][0]['address_components'][3]['long_name']
+    @current_state = place.body['results'][0]['address_components'][5]['short_name']
 
   case @current_condition
       when 'rain'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 WeatherPane::Application.routes.draw do
   root "locations#index"
   get '/locations/:latitude/:longitude' => "locations#show"
-
 end
