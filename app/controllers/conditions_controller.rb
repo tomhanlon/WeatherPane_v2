9bd53127483e2d9640aa0344704f8c5b0f9e203c
Tom Hanlon <tomhanlon@gmail.com>
2013-12-08 01:56:18 -0600

    1:56 am changes

--- app/controllers/conditions_controller.rb
+++ app/controllers/conditions_controller.rb
@@ -2,13 +2,9 @@ class ConditionsController < ApplicationController
 
   def index
 
-  
-
 	response = Unirest.get("https://api.forecast.io/forecast/89c4c48896b556b333f12c324914e8f1/30.2500,-97.7500")
   @current_condition = response.body['currently']['icon']
   @current_temp = response.body['currently']['temperature']
-
-
 
   case @current_condition
   	when 'rain'
